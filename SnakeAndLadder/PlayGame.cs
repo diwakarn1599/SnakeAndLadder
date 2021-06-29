4ac37c190e3c40f5c63fb268b9a10a22d807393e
Diwakar <diwakarn1599@gmail.com>
2021-06-29 13:35:19 +0530

    [Diwakar] usecase 3 - checking option and moving player based on that

--- SnakeAndLadder/PlayGame.cs
+++ SnakeAndLadder/PlayGame.cs
@@ -9,19 +9,43 @@ namespace SnakeAndLadder
     {
         //initialising constant variable
         public const int START_POSITION = 0;
+        public const int NO_PLAY = 1;
+        public const int LADDER = 2;
+        public const int SNAKE = 3; 
+
 
         //Method for start game
         public void StartGame()
         {
             //Console.WriteLine("Player position starts with "+ START_POSITION);
-            int diceRoll;
+            int diceRoll,checkOption,playerPosition = START_POSITION;
+
             //initialisng random class
             Random rand = new Random();
 
             //Generating random number for diceRoll
             diceRoll = rand.Next(1, 7);
 
+            //generating random for checking options
+            checkOption = rand.Next(1, 4);
+
+            //Console.WriteLine(checkOption);
+            switch (checkOption)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    playerPosition += diceRoll;
+                    break;
+                case SNAKE:
+                    playerPosition = (playerPosition - diceRoll) < 0 ? 0 : (playerPosition - diceRoll);
+                    break;
+                default:
+                    break;
+            }
+            
             Console.WriteLine("The player throws "+ diceRoll);
+            Console.WriteLine("The player Position" + playerPosition);
 
 
 
