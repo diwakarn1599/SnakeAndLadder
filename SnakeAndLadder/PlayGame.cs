1f38484400699f22cfede97dc118630274240c0a
Diwakar <diwakarn1599@gmail.com>
2021-06-29 12:43:58 +0530

    [Diwakar] Usecase 1 - Player position start with 0

---
+++ SnakeAndLadder/PlayGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    
+    class PlayGame
+    {
+        public const int START_POSITION = 0;
+
+
+        public void StartGame()
+        {
+            Console.WriteLine("Player position starts with "+ START_POSITION);
+        }
+
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -2,11 +2,22 @@
 
 namespace SnakeAndLadder
 {
+    /// <summary>
+    ///  Code for Snake and Ladder  Game 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //welcome message
             Console.WriteLine("Welcome to Snake and ladder problem");
+
+            //initializing object for playgame class
+            PlayGame pg = new PlayGame();
+
+            //calling a method using object
+            pg.StartGame();
+
         }
     }
 }
