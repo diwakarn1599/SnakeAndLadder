8d1c3a5d852f7cb73922ab358b3f69874020b72e
Diwakar <diwakarn1599@gmail.com>
2021-06-29 12:56:56 +0530

    [Diwakar] Usecase 2 - Player rolls dice b/w 1 to 6

--- SnakeAndLadder/PlayGame.cs
+++ SnakeAndLadder/PlayGame.cs
@@ -7,12 +7,26 @@ namespace SnakeAndLadder
     
     class PlayGame
     {
+        //initialising constant variable
         public const int START_POSITION = 0;
 
-
+        //Method for start game
         public void StartGame()
         {
-            Console.WriteLine("Player position starts with "+ START_POSITION);
+            //Console.WriteLine("Player position starts with "+ START_POSITION);
+            int diceRoll;
+            //initialisng random class
+            Random rand = new Random();
+
+            //Generating random number for diceRoll
+            diceRoll = rand.Next(1, 7);
+
+            Console.WriteLine("The player throws "+ diceRoll);
+
+
+
+
+
         }
 
     }
